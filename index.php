a3602243250c4f3667fdb584d423966c56128017
Fathul Muiin <fmuiin14@gmail.com>
2020-12-05 00:05:58 +0700

    1 prepare

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Aplikasi Penjualan Pulsa">
+    <meta name="keywords" content="Aplikasi Penjualan Pulsa">
+    <meta name="author" content="Fathul Muiin belajar dari Indra Styawantoro">
+
+    
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/plugins/bootstrap-4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/plugins/DataTables/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="assets/plugins/datepicker/css/datepicker.min.css">
+    <link rel="stylesheet" href="assets/plugins/fontawesome-free-5.5.0-web/css/all.min.css">
+    <link rel="stylesheet" href="assets/plugins/sweetalert/css/sweetalert.css">
+    <link rel="stylesheet" href="assets/plugins/choosen-bootstrap-4/css/choosen.css">
+
+    <!-- Custom css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+    <!-- jquery -->
+    <script src="assets/js/jquery-3.3.1.js"></script>
+
+
+    <title>Data Transaksi Penjualan</title>
+
+</head>
+<body>
+    
+</body>
+</html>
